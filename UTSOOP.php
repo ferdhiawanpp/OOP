ceb6bdc40506b8d763ba46c7476e7bfca83543bf
Ferdhiawan <47802437+ferdhiawanpp@users.noreply.github.com>
2019-10-15 14:58:15 +0700

    Add files via upload

---
+++ UTSOOP.php
@@ -0,0 +1,91 @@
+<?php
+
+abstract class pegawai
+{
+	protected $nama;
+	protected $nip;
+	protected $alamat;
+	
+	function __construct($nama,$nip,$alamat)
+	{
+		$this -> nama = $nama;
+		$this -> nip = $nip;
+		$this -> alamat = $alamat;
+	}
+
+	function getNama()
+	{
+		return $this -> nama;
+	}
+
+	function getNip()
+	{
+		return $this -> nip;
+	}
+
+	function getAlamat()
+	{
+		return $this -> alamat;
+	}
+}
+
+class dosen extends pegawai
+{
+	private $totaldosen;
+	
+	function gajidosen($sks)
+	{
+		$this -> totaldosen = 1500000 * $sks;
+	}
+
+	function getTotaldosen()
+	{
+		return $this -> totaldosen ;
+	}
+}
+
+class staff extends pegawai
+{
+	private $totalstaff;
+	
+	function gajistaff($hari)
+	{
+		$this -> totalstaff = 1500000 * $hari;
+	}
+
+	function getTotalstaff()
+	{
+		return $this -> totalstaff ;
+	}
+}
+
+$dosen= new dosen("ferdhi","18050","coba1");
+$staff= new staff("awan","18060","coba2");
+
+$hari = $_POST['hari'];
+$sks = $_POST['sks'];
+
+$dosen -> gajidosen($sks);
+$staff -> gajistaff($hari);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="" method="post">
+	<input type="text" name="sks" placeholder="sks">
+	<input type="submit" name="">
+	<input type="text" name="" value="<?php echo $dosen -> getTotaldosen();?>" readonly="readonly">
+</form>
+
+<form action="" method="post">
+	<input type="text" name="hari" placeholder="hari">
+	<input type="submit" name="">
+	<input type="text" name="" value="<?php echo $staff -> getTotalstaff();?>" readonly="readonly">
+</form>
+</body>
+</html>
